bb2ac45addbcd19fbe46fdcb0affb164932352d6
Jack Holloway <jack@mrprimate.co.uk>
2022-08-14 21:31:05 +0100

    Remove some vars

--- munch/utils.js
+++ munch/utils.js
@@ -134,13 +134,13 @@ function downloadFile(url, destination, timeout = 1500, count = 0, maxcount = 5,
 }
 
 function randomString(length, chars) {
-  var mask = "";
+  let mask = "";
   if (chars.indexOf("a") > -1) mask += "abcdefghijklmnopqrstuvwxyz";
   if (chars.indexOf("A") > -1) mask += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   if (chars.indexOf("#") > -1) mask += "0123456789";
   if (chars.indexOf("!") > -1) mask += "~`!@#$%^&*()_+-={}[]:\";'<>?,./|\\";
-  var result = "";
-  for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+  let result = "";
+  for (let i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
   return result;
 }
 
