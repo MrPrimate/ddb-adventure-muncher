851f1a8505f6e33ab694a534bcf6e1345f99aa94
Jack Holloway <jack@mrprimate.co.uk>
2022-09-04 15:02:11 +0100

    Improve file logging

--- munch/logger.js
+++ munch/logger.js
@@ -6,6 +6,11 @@ function clearLogFile(){
   logger.transports.file.getFile().clear();
 }
 
+// level: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly'
+function setLogLevel(level = "silly") {
+  logger.transports.file.level = level;
+}
+
 exports.clear = clearLogFile;
 exports.logger = logger;
 exports.info = logger.info;
@@ -13,3 +18,5 @@ exports.warn = logger.warn;
 exports.error = logger.error;
 exports.debug = logger.debug;
 exports.verbose = logger.verbose;
+exports.silly = logger.silly;
+exports.setLogLevel = setLogLevel;
